ddec39db5fbe59857bb0519d67df0a58923ac704
tawfeeq <119751359+tawfeeqahmadkhan@users.noreply.github.com>
2025-01-28 10:54:03 +0530

    Delete settrox-admin directory

--- settrox-admin/src/components/common/AnimatedContent.jsx
+++
@@ -1,5 +0,0 @@
-const AnimatedContent = ({ children }) => {
-  return <div className="tab tab-enter">{children}</div>;
-};
-
-export default AnimatedContent;
--- settrox-admin/src/services/VideoServices.js
+++
@@ -1,8 +0,0 @@
-import requests from "./httpService";
-
-const VideoServices = {
-  getVideo: async () => requests.get("/siteSetting/video"),
-  updateVideo: async (body) => requests.put("/siteSetting/video", body),
-};
-
-export default VideoServices;
